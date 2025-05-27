c257d523cefa2bb28e1711f1855d7c2d43b039d4
yazdan2014 <68695802+yazdan2014@users.noreply.github.com>
2025-05-27 19:20:27 +0330

    adding server

---
+++ server.js
@@ -0,0 +1,13 @@
+import { createServer } from 'http';
+import next from 'next';
+
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on port ${process.env.PORT || 3000}`);
+  });
+});
